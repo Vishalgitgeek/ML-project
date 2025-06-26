2a79541478914326c172efe2300204b05bb95f08
Vishal <vishal29.ku@gmail.com>
2025-06-26 13:08:05 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# it would return retquirements list
+def get_requirements(file_path:str)->list[str]:
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [requirement.replace("\n", "") for requirement in requirements]
+
+        return requirements
+
+
+setup(
+    name='Mlproject',
+    version='0.0.1',
+    author='Vishal Kumar',
+    author_email='vishal29.ku@gmail.com',
+    description='A sample Python package',
+    packages= get_requirements('requirements.txt'),
+    install_requires=[
+        'numpy', 'pandas', 'seaborn'
+    
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
