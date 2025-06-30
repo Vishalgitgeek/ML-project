40ab5861fe7c39370397578496f4aa78e46ce2f7
Vishal <vishal29.ku@gmail.com>
2025-06-30 22:16:30 +0530

    defining project structure, added logging and exception

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # it would return retquirements list
 def get_requirements(file_path:str)->list[str]:
---
+++ src/exception.py
@@ -0,0 +1,32 @@
+import sys
+import logging
+from logger import *
+
+def error_message_details(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name =exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = "error in script : [{0}] line number [{1}] error message [{2}]".format(file_name, line_number, str(error))
+
+    return error_message
+
+
+class Custom_Exception(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+# testing purpose
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         try:
+#             raise Custom_Exception(e, sys)
+#         except Custom_Exception as ce:
+#             logging.error(ce)
+        
